cc12dec84f50ec6f1160985a06a4ca7c493f0007
Rohit <rohitnishad123467@gmail.com>
2026-02-18 16:55:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 const server = http.createServer(app)
 const io = new Server(server,{
   cors:{
-    origin: "http://localhost:5173",
+    origin: "https://vingo-food-delivery-zqlo.onrender.com",
     credentials: true,
     methods:['POST','GET'] 
   }
