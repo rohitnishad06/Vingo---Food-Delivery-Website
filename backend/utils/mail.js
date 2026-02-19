6533465b94ad4eb4d7da2d7828e572a4efeee060
Rohit <rohitnishad123467@gmail.com>
2026-02-19 12:02:00 +0530

    Update mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -15,6 +15,15 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+// Verify connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log("Transporter Verification Error:", error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
 export const sendOtpMail = async(to,otp) => {
   await transporter.sendMail({
     from: process.env.EMAIL,
